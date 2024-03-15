3f187b789e1e11b58829c0ad86f52e763ec36b90
Samiksha <swabhimankhadka2001@gmail.com>
2024-03-15 15:12:35 +0545

    feat(database): database conncetion string

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://samikshakhadka0:Microsoft@online-book-shop.vmcjbhr.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Online-Book-Shop";
--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT} `);
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to Book Store");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
